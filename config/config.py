cf62298201e2a9ff28fc37c4d985ee3914565583
walczykk <walkowiczjj@gmail.com>
2025-09-22 17:16:42 +0200

    Project structure

---
+++ config/config.py
@@ -0,0 +1,34 @@
+BOOTSTRAP_SERVER = "localhost:9094, localhost:9095, localhost:9096"
+
+INIT_CLIENT_CONFIG = {
+    "bootstrap.servers": BOOTSTRAP_SERVER
+}
+
+RAW_MSG_TOPIC = "raw-messages"
+MSG_WITH_EMBEDDINGS_TOPIC = "messages-with-embeddings"
+
+PARTITIONS_NUM = 3
+REPLICATION_FACTOR_NUM = 3
+
+
+BATCH_SIZE = 64
+DATASET_FILE_PATH = "kaggle-twitter-dataset/twitter_dataset_1000.csv"
+EMBEDDING_MODEL = "all-MiniLM-L6-v2"
+
+DATASET_PARAMS = {
+    "filepath_or_buffer": DATASET_FILE_PATH,
+    "sep": ",",
+    "usecols": ["text"],
+    "dtype": {"text": "str"}
+}
+
+PRODUCER_CONFIG={
+    "bootstrap.servers": BOOTSTRAP_SERVER
+}
+
+CONSUMER_CONFIG = {
+    "bootstrap.servers": BOOTSTRAP_SERVER,
+    "group.id": "embeddings-client",
+    "auto.offset.reset": "earliest",
+    "enable.auto.commit": False
+}
