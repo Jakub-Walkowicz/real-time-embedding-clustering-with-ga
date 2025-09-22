8f55ec9e52cf85ebca57861bfec3fb078f63f95b
walczykk <walkowiczjj@gmail.com>
2025-09-22 18:35:28 +0200

    Started dev of genetic algorithm

--- genetic_algorithm/genetic_algorithm.py
+++ genetic_algorithm/genetic_algorithm.py
@@ -1,37 +1,46 @@
-from config.config import CONSUMER_CONFIG, MSG_WITH_EMBEDDINGS_TOPIC
+from config.config import CONSUMER_CONFIG, MSG_WITH_EMBEDDINGS_TOPIC, BATCH_SIZE
 from confluent_kafka import Consumer, KafkaError
 import json
+import numpy
 
 consumer = Consumer(CONSUMER_CONFIG)
 consumer.subscribe([MSG_WITH_EMBEDDINGS_TOPIC])
 
-batch = []
+
+messages_decoded = []
+embeddings = []
 running = True
 
-while running:
-    try:
-        event = consumer.poll(1.0)
+try:
+    while running:
+        batch = consumer.consume(BATCH_SIZE, 5.0)
         
-        if not event:
+        if not batch:
             continue
         
-        if event.error():
-            if event.error().code() == KafkaError._PARTITION_EOF:
-                print(f"Reached end of the partition: {event.topic()} [{event.partition()}]")
-            else:
-                print(f"Consumer error: {event.error()}")
-            continue
+        for event in batch:
+            # Damaged events
+            if event.error():
+                if event.error().code() == KafkaError._PARTITION_EOF:
+                    print(f"Reached end of the partition: {event.topic()} [{event.partition()}]")
+                else:
+                    print(f"Consumer error: {event.error()}")
+                continue
+            
+            try:
+                msg_decoded = json.loads(event.value().decode("utf-8"))
+                messages_decoded.append(msg_decoded)
+                embeddings.append(msg_decoded['embeddings'])
+            except(json.JSONDecodeError, KeyError) as e:
+                print((f"Could not parse the JSON message: {e}. Message: {event.value()}"))
+
+        # Reduce the number of dimensions
         
-        try:
-            msg_decoded = json.loads(event.value().decode("utf-8"))      
-        except(json.JSONDecodeError, KeyError) as e:
-            print((f"Could not parse the JSON message: {e}. Message: {event.value()}"))
-            continue
         
-        print(msg_decoded)
         
-    except KeyboardInterrupt:
-        print("Stopping listener...")
-    finally:
-        print('Closing consumer...')
-        consumer.close()
+        
+except KeyboardInterrupt:
+    print("Stopping listener...")
+finally:
+    print('Closing consumer...')
+    consumer.close()
